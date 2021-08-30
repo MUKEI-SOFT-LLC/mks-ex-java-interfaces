a8c8ced148ba07c7aa5fee2d6268b0eb033b7870
om-mks <murayama@mukei-soft.co.jp>
2021-08-31 03:23:53 +0900

    initial commit.

---
+++ protobuf.sh
@@ -0,0 +1,10 @@
+readonly INTERFACE_SRC_DIR="../mks-ex-proto-interfaces/src/proto/"
+readonly PLUGIN_PATH="/Users/omurayama/Downloads/protoc-gen-grpc-java-1.40.1-osx-x86_64.exe"
+readonly SRC_DIR=./protobuf/src/main/java
+for p in `find $INTERFACE_SRC_DIR -name '*.proto'` 
+do
+  relative_path=`echo $p | sed "s@$INTERFACE_SRC_DIR@@g" | sed 's@^/@@'`
+  echo "target file is $relative_path"
+  protoc --plugin=protoc-gen-grpc-java=$PLUGIN_PATH \
+        --proto_path=$INTERFACE_SRC_DIR --grpc-java_out=$SRC_DIR --java_out=$SRC_DIR $relative_path
+done
