e0215cc3e8c236469d0fed59cd1a7c1efdfc01da
om-mks <murayama@mukei-soft.co.jp>
2021-08-31 11:50:22 +0900

    rename proto_src_dir and refactor

--- protobuf.sh
+++ protobuf.sh
@@ -1,10 +1,10 @@
-readonly INTERFACE_SRC_DIR="../mks-ex-proto-interfaces/src/proto/"
+readonly PROTO_SRC_DIR="../mks-ex-proto-defs/src/proto/"
 readonly PLUGIN_PATH=`which protoc-gen-grpc-java-1.40.1-osx-x86_64.exe`
 readonly SRC_DIR=./protobuf/src/main/java
-for p in `find $INTERFACE_SRC_DIR -name '*.proto'` 
+for p in `find $PROTO_SRC_DIR -name '*.proto'` 
 do
-  relative_path=`echo $p | sed "s@$INTERFACE_SRC_DIR@@g" | sed 's@^/@@'`
+  relative_path=`echo $p | sed "s@$PROTO_SRC_DIR@@g" | sed 's@^/@@'`
   echo "target file is $relative_path"
   protoc --plugin=protoc-gen-grpc-java=$PLUGIN_PATH \
-        --proto_path=$INTERFACE_SRC_DIR --grpc-java_out=$SRC_DIR --java_out=$SRC_DIR $relative_path
+        --proto_path=$PROTO_SRC_DIR --grpc-java_out=$SRC_DIR --java_out=$SRC_DIR $relative_path
 done
